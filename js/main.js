01bca3dea6f17c393cd6f4c142bb18dea07219f5
sara <saraahmad2060039@gmail.com>
2023-01-18 17:37:08 +0200

    changes in  js file

--- js/main.js
+++ js/main.js
@@ -4,18 +4,30 @@ const featuresContenet = document.querySelector(".features .content");
 const eachCardWidth = document.querySelector(
   ".features .content .card"
 ).clientWidth;
+
 const eachCardleft = document.querySelector(".features .content .card");
-console.log(featuresContenet.clientWidth);
+const eachCard = document.querySelector(".features .content .card");
+
 let count = 30;
 
 leftArrow.addEventListener("click", () => {
   count += 200;
-
-  featuresContenet.style.transform = `translateX(-${eachCardWidth + count}px)`;
+  console.log(featuresContenet.style.transform);
+  if (eachCardWidth + count > 2050) {
+    count = 30;
+    featuresContenet.style.transform = `translateX(0)`;
+  } else {
+    featuresContenet.style.transform = `translateX(-${
+      eachCardWidth + count
+    }px)`;
+  }
 });
 rightArrow.addEventListener("click", () => {
   count -= 150;
-
   console.log(featuresContenet.style.transform);
+  if (eachCardWidth - count >= -10) {
+    count = 30;
+    featuresContenet.style.transform = `translateX(0)`;
+  }
   featuresContenet.style.transform = `translateX(${eachCardWidth - count}px)`;
 });
